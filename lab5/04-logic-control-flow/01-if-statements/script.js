7902be6fb8fbfdc52490e32951539f05cf047cbb
“nanthawat” <nan292546@gmail.com>
2025-03-10 12:01:46 +0700

    git commit

--- lab5/04-logic-control-flow/01-if-statements/script.js
+++ lab5/04-logic-control-flow/01-if-statements/script.js
@@ -1,4 +1,3 @@
-// If Statement Syntax
 if (true) {
   console.log('This is true');
 }
@@ -7,7 +6,6 @@ if (false) {
   console.log('This is NOT true');
 }
 
-// Evaluation expressions
 const x = 10;
 const y = 5;
 
@@ -21,15 +19,13 @@ if (x === y) {
   console.log(`${x} is NOT equal to ${y}`);
 }
 
-// Block scope
 if (x !== y) {
   const z = 20;
   console.log(`${z} is 20`);
 }
 
-console.log(z); // Throw error
+console.log(z); 
 
-// Shorthand If/Else
 if (x >= y)
   console.log(`${x} is greater than or equal to ${y}`),
     console.log('This is true');
